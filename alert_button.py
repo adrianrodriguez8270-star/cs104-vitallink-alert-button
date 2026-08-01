491e05a81bafc88173acdf5a5e7846df4be0de7f
adrian <adrianrodriguez8270@gmail.com>
2026-08-01 13:18:35 -0600

    init commit

---
+++ alert_button.py
@@ -0,0 +1,9 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+    if GPIO.input(7) == GPIO.HIGH:
+        print("Someone pressed the alert button!")
+    time.sleep(0.1)
